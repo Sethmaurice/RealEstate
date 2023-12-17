5c3f32003675a89e59fa152930da0b88f68cee02
Sethmaurice <101460323+Sethmaurice@users.noreply.github.com>
2023-12-17 10:23:40 +0200

    cors

--- backend/realestate/src/main/java/com/example/realestate/cors/CorsConfig.java
+++ backend/realestate/src/main/java/com/example/realestate/cors/CorsConfig.java
@@ -15,29 +15,45 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://realestate-neon-mu.vercel.app", "http://localhost:3000", "https://realestate-qfhq.onrender.com")
+                        .allowedMethods("*") // Allow all HTTP methods
+                        .allowedHeaders("*") // Allow all headers
+                        .allowCredentials(true); // some changes
+            }
+        };
+    }
 
-        // Allow specific origins
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//
+//        // Allow specific origins
 //        config.addAllowedOrigin("http://localhost:3000"); // Update with your frontend URL
 //        config.addAllowedOrigin("https://realestate-qfhq.onrender.com");
-        config.addAllowedOrigin("https://realestate-neon-mu.vercel.app");
-
-        // Allow specific methods
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("DELETE");
-        config.getAllowCredentials();
-
-        // Allow specific headers
-        config.addAllowedHeader("*");
-
-        source.registerCorsConfiguration("/**", config);
-
-        return new CorsFilter(source);
-    }
+//        config.addAllowedOrigin("https://realestate-neon-mu.vercel.app");
+//        config.addAllowedMethod("*");
+//        config.addAllowedHeader("*");
+////
+//
+//        // Allow specific methods
+//        config.addAllowedMethod("GET");
+//        config.addAllowedMethod("POST");
+//        config.addAllowedMethod("PUT");
+//        config.addAllowedMethod("DELETE");
+//        config.getAllowCredentials();
+//
+//        // Allow specific headers
+//        config.addAllowedHeader("*");
+//
+//        source.registerCorsConfiguration("/**", config);
+//
+//        return new CorsFilter(source);
+//    }
 }
